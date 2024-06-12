0ae3798a54c77a2e6920bbc3a0f761b7374babce
yonasghiwot <yonihabeshaw@gmail.com>
2024-06-12 21:21:16 +0300

    12-binary_tree_leaves.c

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_leaves: counts leaves
+ * binary_tree_leaves: function that checks if a binary tree is full
  * @tree: tree of leaves to be counted
  * Return: num of leaves
- *         0 == NULL
+ *        or 0 NULL if tree is NULL
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
